e4a350c4bf55a52f3e85c6df03c4d5087a46ec43
Craig Corcoran <craig@newknowledge.io>
2018-05-13 10:05:37 -0500

    created a mess while testing different generators for downloading adl data

--- azure_utils/downloader.py
+++ azure_utils/downloader.py
@@ -31,6 +31,38 @@ def get_datalake_client(store_name=None, tenant_id=None, client_id=None, client_
     return core.AzureDLFileSystem(token, store_name=store_name)
 
 
+def yield_documents_df(json_string, include_retweets=False, return_batches=False, keep_cols=KEEP_KEYS):
+    # vprint = print
+    file_df = pd.read_json(json_string, lines=True, orient='records')
+    # if keep_cols specified, drop all other cols
+    file_df = file_df[keep_cols] if keep_cols else file_df
+
+    if not include_retweets:
+        n_rows = len(file_df)
+        # vprint('dropping retweets')
+        file_df = file_df[file_df['connectionType'] != 'retweet']
+        # vprint('dropped', n_rows - len(file_df), 'retweets')
+
+    # vprint('yielding', len(file_df), 'samples')
+    if return_batches:
+        yield file_df
+    else:
+        for row in file_df.iterrows():
+            yield row[1]  # iterrows returns (row_num, Series) pair
+
+
+def yield_documents(json_string, include_retweets=False, keep_cols=KEEP_KEYS):
+    json_string = '[' + ','.join(line for line in json_string.splitlines()) + ']'
+    documents = json.loads(json_string)
+    for row in documents:
+        if not include_retweets and row['connectionType'] == 'retweet':
+            continue
+        if keep_cols:
+            yield {k: v for k, v in row.items() if k in keep_cols}
+        else:
+            yield row
+
+
 def dataframe_generator(read_date=date.today(),
                         index='disney',
                         store_name='sociallake',
@@ -38,13 +70,12 @@ def dataframe_generator(read_date=date.today(),
                         prefix='/streamsets/prod',
                         keep_cols=None,
                         include_retweets=False,
-                        return_batches=False,
                         verbose=False,
+                        return_batches=False,
+                        use_df=False,
                         ):
     ''' Generator function for reading all data for given date from Azure Data Lake given store and index.
         Converts json files into rows or batches of pandas dataframes. '''
-
-    # TODO remove nan, do other common prep (in batches before yielding)?
 
     vprint = print if verbose else lambda x: None
     if isinstance(read_date, date):
@@ -56,7 +87,7 @@ def dataframe_generator(read_date=date.today(),
 
     vprint('finding files')
     # list out files for that day
-    files = client.ls("{0}/{1}/{2}".format(prefix, index, read_date))
+    files = client.ls("{0}/{1}/{2}".format(prefix, index, read_date))[:3]
     for i, filename in enumerate(files):
         vprint('file', i+1, 'of', len(files))
 
@@ -66,25 +97,38 @@ def dataframe_generator(read_date=date.today(),
 
         try:
             with client.open(filename, 'rb') as adl_file:
-                # read json string from adl file
+                # read json string from adl file, parse into docs and yield rows
                 json_string = adl_file.read().decode(encoding='utf-8')
-                file_df = pd.read_json(json_string, lines=True, orient='records')
 
-                # if keep_cols specified, drop all other cols
-                file_df = file_df[keep_cols] if keep_cols else file_df
+                if use_df:
+                    file_df = pd.read_json(json_string, lines=True, orient='records')
+                    # if keep_cols specified, drop all other cols
+                    file_df = file_df[keep_cols] if keep_cols else file_df
 
-                if not include_retweets:
-                    n_rows = len(file_df)
-                    vprint('dropping retweets')
-                    file_df = file_df[file_df['connectionType'] != 'retweet']
-                    vprint('dropped', n_rows - len(file_df), 'retweets')
+                    if not include_retweets:
+                        n_rows = len(file_df)
+                        vprint('dropping retweets')
+                        file_df = file_df[file_df['connectionType'] != 'retweet']
+                        vprint('dropped', n_rows - len(file_df), 'retweets')
 
-                vprint('yielding', len(file_df), 'samples')
-                if return_batches:
-                    yield file_df
+                    vprint('yielding', len(file_df), 'samples')
+                    if return_batches:
+                        yield file_df
+                    else:
+                        for row in file_df.iterrows():
+                            yield row[1]  # iterrows returns (row_num, Series) pair
                 else:
-                    for row in file_df.iterrows():
-                        yield row[1]  # iterrows returns (row_num, Series) pair
+                    json_string = '[' + ','.join(line for line in json_string.splitlines()) + ']'
+                    documents = json.loads(json_string)
+                    for row in documents:
+                        if not include_retweets and row['connectionType'] == 'retweet':
+                            continue
+                        if keep_cols:
+                            yield {k: v for k, v in row.items() if k in keep_cols}
+                        else:
+                            yield row
+
+                # TODO remove nan, do other common prep (in batches before yielding)?
 
         except RuntimeError as err:
             vprint(err)
@@ -177,7 +221,51 @@ def upload_datalake_file(remote_path='test.txt', local_path='./test.txt', store_
                             nthreads=4, overwrite=overwrite, buffersize=2**24, blocksize=2**24)
 
 
-def test_dataframe_generator():
+def test_data_clean(index='disney',
+                    store_name='sociallake',
+                    envfile='datalake.env',
+                    prefix='/streamsets/prod'):
+
+    read_date = date.today()
+    client = get_datalake_client(store_name=store_name, envfile=envfile)
+    vprint = print
+    vprint('finding files')
+    # list out files for that day
+    files = client.ls("{0}/{1}/{2}".format(prefix, index, read_date))[:6]
+    json_strings = []
+    for i, filename in enumerate(files):
+        vprint('file', i+1, 'of', len(files))
+
+        if '_tmp' in filename:
+            vprint('skipping temp file', filename)
+            continue
+
+        with client.open(filename, 'rb') as adl_file:
+            # read json string from adl file, parse into docs and yield rows
+            json_string = adl_file.read().decode(encoding='utf-8')
+            json_strings.append(json_string)
+
+    start_time = time.time()
+    full_df = pd.DataFrame(row for row in yield_documents(json_string) for json_string in json_strings)
+    print(full_df.shape)
+    print('rows without df took time', time.time()-start_time, '\n\n')
+
+    start_time = time.time()
+    full_df = pd.DataFrame(row for row in yield_documents_df(json_string, return_batches=False)
+                           for json_string in json_strings)
+    print(full_df.shape)
+    print('df rows took time', time.time()-start_time, '\n\n')
+
+    start_time = time.time()
+    full_df = pd.concat(row for row in yield_documents_df(json_string, return_batches=True)
+                        for json_string in json_strings)
+    print(full_df.shape)
+    print('df batches took time', time.time()-start_time, '\n\n')
+
+    # full_df = pd.DataFrame(df for df in data_batch_gen)
+
+
+def test_dataframe_generator(read_date):
     data_row_gen = dataframe_generator(
         read_date=date.today(),
         index='disney',
@@ -187,7 +275,13 @@ def test_dataframe_generator():
         keep_cols=KEEP_KEYS,
         include_retweets=False,
         verbose=True,
+        use_df=True,
     )
+
+    print('create dataframe from use_df row generator')
+    start_time = time.time()
+    full_df = pd.DataFrame(row for row in data_row_gen)
+    print('rows took time', time.time()-start_time)
 
     data_batch_gen = dataframe_generator(
         read_date=date.today(),
@@ -199,19 +293,34 @@ def test_dataframe_generator():
         include_retweets=False,
         verbose=True,
         return_batches=True,
+        use_df=True,
     )
 
-    print('create dataframe from row generator')
-    start_time = time.time()
-    full_df = pd.DataFrame(row for row in data_row_gen)
-    print('rows took time', time.time()-start_time)
-
-    print(full_df.shape)
-
+    # print(full_df.shape)
     print('concat batches from df generator')
     start_time = time.time()
     full_df = pd.concat(df for df in data_batch_gen)
     print('batches took time', time.time()-start_time)
+
+    data_batch_gen = dataframe_generator(
+        read_date=date.today(),
+        index='disney',
+        store_name='sociallake',
+        envfile='datalake.env',
+        prefix='/streamsets/prod',
+        keep_cols=KEEP_KEYS,
+        include_retweets=False,
+        verbose=True,
+        # return_batches=True,
+        # use_df=True,
+    )
+
+    # print(full_df.shape)
+    print('rows not using dataframe from generator')
+    start_time = time.time()
+    # full_df = pd.DataFrame(df for df in data_batch_gen)
+    full_df = pd.DataFrame(row for row in data_row_gen)
+    print('rows without df took time', time.time()-start_time)
 
     print(full_df.shape)
 
@@ -224,11 +333,13 @@ def test_dataframe_generator():
     full_df.drop_duplicates(inplace=True, subset=str_cols)
 
     print('dropped:', n_rows - len(full_df), 'duplicates')
+    full_df.to_csv(read_date.isoformat() + '_test.csv')
     # TODO dedupe with more column types (list, dict?)
 
 
 if __name__ == '__main__':
-    test_dataframe_generator()
+    # test_dataframe_generator(date.today())
+    test_data_clean()
     # get_social_data(date=date.today() - timedelta(days=1), index='disney')
     # print(list_social_indices())
 
